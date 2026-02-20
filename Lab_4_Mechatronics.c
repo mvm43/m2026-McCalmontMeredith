76fc3c6c3580be27f4d5893863d898325089e6fb
mvm43 <mvm43@pitt.edu>
2026-02-19 23:34:17 -0500

    Add files via upload

--- Lab_4_Mechatronics.c
+++ Lab_4_Mechatronics.c
@@ -20,7 +20,7 @@ int main(void)
 {
     //Declare the number of states for the lab4 pattern
 	int num_states = 22; // there's 22 states including the 2 second off at end, this is noted as e in the pattern array
-	char pattern[] = {'A','C','B','C','A','C','A','D','A','D','B','C','B','C','A','D','B','C','B','C','B','E'}
+	char pattern[] = {'A','C','B','C','A','C','A','D','A','D','B','C','B','C','A','D','B','C','B','C','B','E'} //Follows the specified pattern in the lab handout
 		
 	// Set inputs and outputs
 	DDRD = 0b00000000;
@@ -31,10 +31,10 @@ int main(void)
 	while (PIND & 0b00000100)
 	{
 	}
-	
+	//While loop for the indefinite loop and changes states in the pattern array for the cases in the wait void loop
     while (1) 
     {
-	for (int i=0; i<num_states; i++)
+	for (int i=0; i<num_states; i++) //counts up the columns the array
 	{
 		change_led_state(pattern[i]);
 	}
@@ -43,22 +43,23 @@ int main(void)
 	return(0);
 }
 
+// Void wait to specify the short/long on and offs
 void change_led_state(char new_state)
 {
 	switch(new_state) {
-		case 'A': 
+		case 'A': //specifies wait for short on period, including pins for port C
 			PORTC = 0b00000000;
 			wait(200);
 			break;
-		case 'B':
+		case 'B': //specifies wait for long on period
 			PORTC = 0b00000000;
 			wait(600);
 			break;
-		case 'C':
+		case 'C': //specifies the wait for short off period, including pins for port C
 			PORTC = 0b00000011;
 			wait(200);
 			break;
-		case 'D':
+		case 'D': // specifies the wait for long off period
 			PORTC = 0b00000011;
 			wait(600)
 			break;
